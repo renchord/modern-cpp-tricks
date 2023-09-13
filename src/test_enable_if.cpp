34088048ce66867f945d85ff26874d902f895378
chordr <chordr@chiplego.com>
2023-09-13 20:04:35 +0800

    Add more template inner enable_if_test

--- src/test_enable_if.cpp
+++ src/test_enable_if.cpp
@@ -30,6 +30,45 @@ typename detail::enable_if<!std::is_trivial<T>::value>::type test_enable_if_matc
     std::cout << "Matched the non-trivial calling function." << std::endl;
 }
 
+namespace detail {
+    void* voidify(const volatile void* ptr) noexcept {
+        return const_cast<void*>(ptr);
+    }
+}
+
+template <typename T>
+typename detail::enable_if<std::is_trivially_default_constructible<T>::value>::type construct(T*) {
+    std::cout << "default constructing trivially default constructible T\n";
+}
+
+template <typename T>
+typename detail::enable_if<!std::is_trivially_default_constructible<T>::value>::type construct(T* p) {
+    std::cout << "default constructing non-trivially default constructible T\n";
+    ::new(detail::voidify(p)) T; //placement new
+}
+
+template <class T>
+void destroy(T*, typename detail::enable_if<std::is_trivially_destructible<T>::value>::type* = 0) {
+    std::cout << "destorying trivially destructible T\n";
+}
+
+template <class T,
+        typename detail::enable_if<
+                !std::is_trivially_destructible<T>::value &&
+                (std::is_class<T>{} || std::is_union<T>{}),
+                bool>::type = true>
+void destroy(T* t) {
+    std::cout << "destorying non-trivially destrtuctible T\n";
+    t->~T(); // placement destruct calling
+}
+
+template <class T, typename = std::enable_if_t<std::is_array<T>::value>>
+void destroy(T* t) {
+    for (size_t i = 0; i < std::extent<T>::value; ++i) {
+        destroy((*t)[i]);
+    }
+}
+
 int main() {
 
     // test1
